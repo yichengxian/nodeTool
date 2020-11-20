6bce151b07576e6ccb85debcfa0152c7402a1efb
yichengxian <yichengxian@Tom.com>
2020-11-20 20:12:52 +0800

    up index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+
+require('./core/util/StringUtil');
+require('./core/util/ObjectUtil');
+require('./core/util/ArrayUtil');
+require('./core/util/PageUtil');
+require('./core/util/RandomUtil');
+require('./core/util/PhoneUtil');
+require('./core/lang/Snowflake');
+//建议使用别的uuid
+require('./core/lang/UUID');
+require('./core/date/DateUtil');
+require('./random/RandomWeightUtil');
