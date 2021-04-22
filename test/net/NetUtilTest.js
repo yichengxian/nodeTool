388930751fe6fbc416a92628f8b2445beadb9b22
yichengxian <13272263301@163.com>
2021-04-22 16:11:43 +0800

    增加 RequestUtil.js

--- test/net/NetUtilTest.js
+++ test/net/NetUtilTest.js
@@ -4,10 +4,16 @@ const NetUtil = require('../../core/net/NetUtil');
 console.info(NetUtil.isValidPort(65536)); //false
 
 //检测端口是否可用 async
-NetUtil.isUsableLocalPort(80).then(res =>console.info(res)); //false
+NetUtil.isUsableLocalPort(801).then(res =>console.info("这个检测端口是否可用:%s",res)); //false
 
 //获取当前ipv4 ip
 console.log(NetUtil.getIPV4Address()); //192.168.4.97
 
 // 获取ipv6地址
 console.info(NetUtil.getIPV6Address()) ;//fe80::407a:a6d8:1623:aa89
+
+//檢測字符串是不是UNKNOWN
+console.info(NetUtil.isUnknown('UNKNOWN'));//true
+
+//从多级反向代理中获得第一个非unknown IP地址
+console.info(NetUtil.getMultistageReverseProxyIp('192.6.12.3,2324.12,31')); //192.6.12.3
